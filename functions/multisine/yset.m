51f60d9d4706a6c39738dd5658df5503275cfaea
mgcth <mladen.gibanica@chalmers.se>
2015-08-04 10:29:38 +0200

    more fixes

---
+++ functions/multisine/yset.m
@@ -0,0 +1,15 @@
+%% ========================================================================
+function yset
+global statGUIg
+Pos=get(statGUIg.fh,'Pos');
+Pos=[Pos(1)+Pos(3)+20 Pos(2) 150 70];
+statGUIg.yseth=figure('Pos',Pos);
+set(statGUIg.yseth,'MenuBar','none','NumberTitle','off');
+set(statGUIg.yseth,'CloseRequestFcn','global statGUIg,set(statGUIg.yseth,''Visible'',''off'');');
+
+for I=1:statGUIg.ny-1,S{I}=['y' int2str(I)];end
+ht=uicontrol(statGUIg.yseth,'Pos',[20 35 120 30],'Style','Text','String','Select Output Channel');
+statGUIg.hpop=uicontrol(statGUIg.yseth,'Pos',[30 10 100 30]);
+set(statGUIg.hpop,'Style','popup','String',S);
+set(statGUIg.hpop,'Call','global statGUIg; statGUIg.ax=[];set(statGUIg.yseth,''Visible'',''off'');if statGUIg.workdone,plotFRD;end');
+
