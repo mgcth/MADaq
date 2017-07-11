9a1951aab8c81bd93e76cdf6d9901f4d509ace61
mgcth <mgcth@users.noreply.github.com>
2017-07-11 11:32:37 +0200

    test
    increase time, but better quality...

--- functions/multisine/startMultisine.m
+++ functions/multisine/startMultisine.m
@@ -70,7 +70,7 @@ nu = length(CH.reference); % number of inputs
 ny = length(CH.active); % number of outputs
 yind = 1:ny; yind(refch) = [];
 nf = length(Freqs);
-blockSize = 3000; % 1500 was good
+blockSize = 4000; % 1500 was good
 
 K = nf/SimFreq; % now TRUE!, not true before when only SimFreq
 Ncyc = 10;
@@ -264,7 +264,7 @@ if ~isempty(multisine.session.Channels) &&  ~isempty(multisine.channelInfo.refer
         %coherenceValue = zeros();
         %while coherenceValue < 0.9 %%% EXPERIMENTAL FEATURE
         coherenceValue = 0;
-        minCoherenceValue = 0.75; % good value around 0.5 - 0.6
+        minCoherenceValue = 0.95; % good value around 0.5 - 0.6
         stepLoadFactor = 1.5; % small increments take longer time (1.25 good)
         maxFactorIncrease = 2.3; % dont want to damadge the shaker (2 good)
         timesIncrease = log(maxFactorIncrease)/log(stepLoadFactor);
